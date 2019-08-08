0109f23b5001ff83d650497fc9f0a5472d5fef4d
zephyrlin <zephrdod@gmail.com>
2019-08-08 14:10:50 +0800

    修了一些bug但是还是没法只运行一遍

--- configZSH.sh
+++ configZSH.sh
@@ -8,35 +8,88 @@ function get_platform_type()
     echo $(uname)
 } 
 
-# 获取linux平台类型，Debian系列还是RedHat系列或者其他
+# 获取linux平台
 function get_linux_platform_type()
 {
-    if which apt-get > /dev/null ; then
-        echo "Debian etc...(Ubuntu/Debian/Deepin etc)"
-    elif which yum > /dev/null ; then
-        echo "RedHat etc...(Fedora/RHEL/CentOS)"
-    elif which pacman > /dev/null ; then
+    if grep -Eq "Ubuntu" /etc/*-release; then
+        echo "Ubuntu"
+    elif grep -Eq "Deepin" /etc/*-release; then
+        echo "Deepin"
+    elif grep -Eq "LinuxMint" /etc/*-release; then
+        echo "LinuxMint"
+    elif grep -Eq "Debian" /etc/*-release; then
+        echo "Debian"
+    elif grep -Eq "CentOS" /etc/*-release; then
+        echo "CentOS"
+    elif grep -Eq "openSUSE" /etc/*-release; then
+        echo "openSUSE"
+    elif grep -Eq "Arch Linux" /etc/*-release; then
         echo "ArchLinux"
     else
-        echo "invalid"
+        echo "Unknow"
     fi
 }
 
 function install_on_linux()
 {
-    type=`get_linux_platform_type`
-    echo "Linux platform type: $type"
-    if [ "$type" -eq "Debian etc...(Ubuntu/Debian/Deepin etc)"] ; then
-        sudo apt-get install zsh curl
-    elif [ "$type" -eq "RedHat etc...(Fedora/RHEL/CentOS)" ] ; then
-        sudo yum install zsh curl
-    elif [ "$type" -eq "ArchLinux" ]
+    linux_type=`get_linux_platform_type`
+    echo "Linux platform type: $linux_type"
+
+    if [[ ${linux_type} == "Ubuntu" ]]; then
+        install_on_ubuntu
+    elif [[ ${linux_type} == "Deepin" ]]; then
+        install_on_ubuntu
+    elif [[ ${linux_type} == "LinuxMint" ]]; then
+        install_on_ubuntu
+    elif [[ ${linux_type} == "Debian" ]]; then
+        install_on_debian
+    elif [[ ${linux_type} == "CentOS" ]]; then
+        install_on_centos
+    elif [[ ${linux_type} == "openSUSE" ]]; then
+        install__on_opensuse
+    elif [[ ${linux_type} == "ArchLinux" ]]; then
+        install_on_archlinux
+    else
+        echo "Not support linux type: "${type}
+    fi
+}
+
+function install_on_ubuntu()
+{
+    sudo apt-get update
+    sudo apt-get install curl zsh
+    install_oh_my_zsh_and_plugins
+}
+
+function install_on_debian()
+{
+    sudo apt-get update
+    sudo apt-get install curl zsh
+    install_oh_my_zsh_and_plugins
+}
+
+function install_on_centos()
+{
+    sudo yum update
+    sudo yum install curl zsh
+    install_oh_my_zsh_and_plugins
+}
+
+function install_on_archlinux()
+{
+    sudo pacman -S zsh curl
+    install_oh_my_zsh_and_plugins
+}
+
+function install_on_opensuse()
+{
+    sudo zypper install zsh curl
     install_oh_my_zsh_and_plugins
 }
 
 function install_oh_my_zsh_and_plugins()
 {
-    fork sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
@@ -55,23 +108,24 @@ function print_logo()
     echo ''
     echo ''
     echo 'Just enjoy it!'
-    echo 'p.s. Follow me at https://github.com/chxuan.'
-    echo 'p.p.s You can copy the .zshrc now!'
+    echo 'p.s. Follow me at https://github.com/zephyrlind.'
+    echo 'p.p.s You can copy the .zshrc now! Or add zsh-syntax-highlighting, zsh-autosuggestions into the .zshrc'
     printf "${normal}"
 }
 
 function main()
 {
-    type = `get_platform_type`
-    echo "Platform type: $type"
-    if [ "$type" -eq "DarWin"] ; then
-        install_oh_my_zsh_and_plugin
-    elif [ "$type" -eq "Linux"] ; then
+    type=`get_platform_type`
+    echo "Platform type: "$type
+    if [[ ${type} == "Darwin" ]]; then
+        install_oh_my_zsh_and_plugin && print_logo
+    elif [[ ${type} == "Linux" ]]; then
         install_on_linux
+        print_logo
     else
         echo "Not Support"
     fi
-    print_logo
+    
 }
 
 # call
